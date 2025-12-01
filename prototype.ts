8afd70f747c8b2ea94f2f623340171d236bfeb93
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-12-01 11:57:39 -0300

    Prototipo

--- prototype.ts
+++ prototype.ts
@@ -2,7 +2,7 @@
 
 -- Objetivo do programa --
 
-Questões de 1 - 5, guardo as notas, a partir disso eu somo as notas e faço a média,
+Questões de 1 - 10, guardo as notas, a partir disso eu somo as notas e faço a média,
 Após isso passo pra uma função que retorna a posição do candidato no nine box de forma visual:
 
 |============|============|============|
@@ -15,6 +15,22 @@ Após isso passo pra uma função que retorna a posição do candidato no nine b
 |            |            |            |
 |            |            |            |
 |============|============|============|
+
+Perguntas de avaliação de desempenho
+
+    • O colaborador atinge as metas e objetivos estabelecidos?
+    • O desempenho é consistente e está acima das expectativas?
+    • Ele entrega o trabalho com qualidade e no prazo?
+    • O profissional demonstra organização e iniciativa em suas tarefas?
+    • Ele lida bem com os desafios e situações inesperadas? 
+
+    Perguntas de avaliação de potencial
+
+    • O colaborador demonstra disposição para aprender e crescer?
+    • Ele busca ativamente oportunidades de desenvolvimento profissional?
+    • Ele se mostra motivado e engajado com a missão da equipe e da empresa?
+    • Ele é receptivo a feedback e os aplica em seu trabalho?
+    • Demonstra potencial de liderança, como aptidão ou desejo de assumir posições de liderança?
 
 */
 import PromptSync from "prompt-sync"; // Biblioteca utilizada para fazer as perguntas [ No código oficial, essa pergunta não vai ser feita no terminal (provavelmente) ]
@@ -41,18 +57,37 @@ const funcionario1 = new funcionario("Rafael", 12389018927, 'RH') // Funcionári
 
 // =========- Funções -========= \\
 
-function perguntas(user: funcionario) {
+function perguntasDesempenho(user: funcionario) {
   console.log(`[LOGIN] Você agora está logado como ${user.nome} de CPF ${user.cpf}`)
-  const questao1 = parseInt(prompt(`Quão boa é sua relação com os outros colaboradores na empresa, ${user.nome}? (1-10): `)) // Exemplo de questão
-  const questao2 = parseInt(prompt(`Quão boa é sua relação com os avaliadores na empresa, ${user.nome}? (1-10): `)) // Exemplo de questão
-  const questao3 = parseInt(prompt(`Quão boa é a infraestrutura da empresa, ${user.nome}? (1-10): `)) // Exemplo de questão
-  const questao4 = parseInt(prompt(`Quão boa é sua relação com os diretores na empresa, ${user.nome}? (1-10): `)) // Exemplo de questão
-  const questao5 = parseInt(prompt(`Quão bom é o fluxo em relação com os outros colaboradores na empresa, ${user.nome}? (1-10): `)) // Exemplo de questão
-  const media = (questao1 + questao2 + questao3 + questao4 + questao5) / 5
-  return Number(media);
+  const questao1 = parseInt(prompt(`O colaborador atinge as metas e objetivos estabelecidos? (0-10): `)) // Exemplo de questão
+  const questao2 = parseInt(prompt(`O desempenho é consistente e está acima das expectativas? (0-10): `)) // Exemplo de questão
+  const questao3 = parseInt(prompt(`Ele entrega o trabalho com qualidade e no prazo? (0-10): `)) // Exemplo de questão
+  const questao4 = parseInt(prompt(`O profissional demonstra organização e iniciativa em suas tarefas? (0-10): `)) // Exemplo de questão
+  const questao5 = parseInt(prompt(`Ele lida bem com os desafios e situações inesperadas? (0-10): `)) // Exemplo de questão
+
+  const mediaDesempenho: number = (questao1 + questao2 + questao3 + questao4 + questao5) / 5
+
+  return (mediaDesempenho);
 }
 
-function notas(nota: typeof notaDoFuncionario){
+perguntasDesempenho(funcionario1)
+
+function perguntasPotencial(user: funcionario){
+  const questao6 = parseInt(prompt(`O colaborador demonstra disposição para aprender e crescer? (0-10): `)) // Exemplo de questão
+  const questao7 = parseInt(prompt(`Ele busca ativamente oportunidades de desenvolvimento profissional? (0-10): `)) // Exemplo de questão
+  const questao8 = parseInt(prompt(`Ele se mostra motivado e engajado com a missão da equipe e da empresa? (0-10): `)) // Exemplo de questão
+  const questao9 = parseInt(prompt(`Ele é receptivo a feedback e os aplica em seu trabalho? (0-10): `)) // Exemplo de questão
+  const questao10 = parseInt(prompt(`Demonstra potencial de liderança, como aptidão ou desejo de assumir posições de liderança? (0-10): `)) // Exemplo de questão
+
+  const mediaPotencial = (questao6 + questao7 + questao8 + questao9 + questao10) / 5
+
+  return (mediaPotencial)
+}
+
+perguntasPotencial(funcionario1)
+
+
+function validacaoNotas(nota: number){
   if (nota <= 6){
     console.log("\n [RESULTADO] Nota abaixo do esperado")    
   } else if (nota > 6 && nota <= 8) {
@@ -62,7 +97,7 @@ function notas(nota: typeof notaDoFuncionario){
   }
 }
 
-function nineBox(desempenho: typeof notaDoFuncionario){
+function nineBox(desempenho: number, potencial: number){
   if (desempenho <= 6){
     console.log(`
     |============|============|============|
@@ -107,6 +142,25 @@ function nineBox(desempenho: typeof notaDoFuncionario){
 
 // =========- Teste das funções -========= \\
 
-const notaDoFuncionario = perguntas(funcionario1)
-notas(notaDoFuncionario)
-nineBox(notaDoFuncionario)
+const desempenho = perguntasDesempenho(funcionario1)
+const potencial = perguntasPotencial(funcionario1)
+
+
+/*
+
+  Para cálculo de desempenho x potencial
+
+      /============|============|============\ Alto
+      |            |            | Sua posição| Potencial
+      |            |            |  é aqui    |
+      |============|============|============|
+      |            |            |            | Médio 
+      |            |            |            | Potencial
+      |============|============|============|
+      |            |            |            | Baixo
+      |            |            |            | Potencial
+      \============|============|============/
+        Baixo         Médio         Alto
+        Desempenho    Desempenho    Desempenho
+
+*/
